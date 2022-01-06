b91a14cd1c867f423dce596c1763b47587279bdf
Jeremy <than2314@gmail.com>
2022-01-07 00:14:21 +0700

    :tada: : initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-white dark:bg-black">
+			<h1 className="text-xs font-bold underline dark:text-white">
+				Hello world!
+			</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+	content: [
+		"./src/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		fontSize: {
+			'poom': '20rem',
+		},
+		extend: {},
+	},
+	plugins: [],
+}
